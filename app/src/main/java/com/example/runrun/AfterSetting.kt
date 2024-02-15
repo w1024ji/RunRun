61c2487cd96f5b404e93dbd0f647a0e658b1a747
Wonji Lee <w1024ji@gmail.com>
2024-02-15 22:01:40 +0900

    CREATE AfterSetting.kt and activity_after_setting.xml
    Because I finished building a main structure - user input of bus and station name,
    show two marks using Google Maps so the user can choose one, and finally
    setting an customized alarm - now it's time for adding further
    functions. Also, I am praticing with git flow. (Just made some branches)
    Anyway, I'm trying to add some user interfaces of RunRun, such as a
    layout after setting a notification, or main page (before entering bus
    and station name)

---
+++ app/src/main/java/com/example/runrun/AfterSetting.kt
@@ -0,0 +1,11 @@
+package com.example.runrun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AfterSetting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_setting)
+    }
+}
